de26fd7e451a4c554628ccc1ce02fc6205147474
cosmolirainmetrics <cosmo.lira@inmetrics.com.br>
2018-01-05 16:35:55 -0200

    Adicionei o Manual

--- TestManager/frmFeature.Designer.cs
+++ TestManager/frmFeature.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmFeature));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnSelecionar = new System.Windows.Forms.Button();
             this.lblCaminho = new System.Windows.Forms.Label();
@@ -48,129 +49,90 @@
             // 
             // groupBox1
             // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Controls.Add(this.btnSelecionar);
             this.groupBox1.Controls.Add(this.lblCaminho);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(645, 50);
-            this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Planilha a ser importada";
             // 
             // btnSelecionar
             // 
+            resources.ApplyResources(this.btnSelecionar, "btnSelecionar");
             this.btnSelecionar.Image = global::TestManager.Properties.Resources._7183_32x32;
-            this.btnSelecionar.Location = new System.Drawing.Point(532, 7);
             this.btnSelecionar.Name = "btnSelecionar";
-            this.btnSelecionar.Size = new System.Drawing.Size(108, 42);
-            this.btnSelecionar.TabIndex = 1;
-            this.btnSelecionar.Text = "&Selecionar";
-            this.btnSelecionar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnSelecionar.UseVisualStyleBackColor = true;
             this.btnSelecionar.Click += new System.EventHandler(this.btnSelecionar_ClickAsync);
             // 
             // lblCaminho
             // 
-            this.lblCaminho.AutoSize = true;
-            this.lblCaminho.Location = new System.Drawing.Point(7, 20);
+            resources.ApplyResources(this.lblCaminho, "lblCaminho");
             this.lblCaminho.Name = "lblCaminho";
-            this.lblCaminho.Size = new System.Drawing.Size(16, 13);
-            this.lblCaminho.TabIndex = 0;
-            this.lblCaminho.Text = "...";
             // 
             // groupBox2
             // 
+            resources.ApplyResources(this.groupBox2, "groupBox2");
             this.groupBox2.Controls.Add(this.pcbAguarde);
             this.groupBox2.Controls.Add(this.dgvFeatures);
-            this.groupBox2.Location = new System.Drawing.Point(12, 68);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(645, 164);
-            this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Features";
             // 
             // pcbAguarde
             // 
-            this.pcbAguarde.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pcbAguarde.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            resources.ApplyResources(this.pcbAguarde, "pcbAguarde");
             this.pcbAguarde.Image = global::TestManager.Properties.Resources.carregando_pacotes;
-            this.pcbAguarde.Location = new System.Drawing.Point(186, 86);
             this.pcbAguarde.Name = "pcbAguarde";
-            this.pcbAguarde.Size = new System.Drawing.Size(304, 78);
-            this.pcbAguarde.TabIndex = 1;
             this.pcbAguarde.TabStop = false;
-            this.pcbAguarde.Visible = false;
             // 
             // dgvFeatures
             // 
+            resources.ApplyResources(this.dgvFeatures, "dgvFeatures");
             this.dgvFeatures.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvFeatures.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvFeatures.Location = new System.Drawing.Point(3, 16);
             this.dgvFeatures.MultiSelect = false;
             this.dgvFeatures.Name = "dgvFeatures";
             this.dgvFeatures.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvFeatures.Size = new System.Drawing.Size(639, 145);
-            this.dgvFeatures.TabIndex = 0;
             this.dgvFeatures.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvFeatures_CellContentClick);
             this.dgvFeatures.SelectionChanged += new System.EventHandler(this.dgvFeatures_SelectionChanged);
             // 
             // groupBox3
             // 
+            resources.ApplyResources(this.groupBox3, "groupBox3");
             this.groupBox3.Controls.Add(this.dgvCenario);
-            this.groupBox3.Location = new System.Drawing.Point(15, 238);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(645, 177);
-            this.groupBox3.TabIndex = 3;
             this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Cenários por Feature";
             // 
             // dgvCenario
             // 
+            resources.ApplyResources(this.dgvCenario, "dgvCenario");
             this.dgvCenario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCenario.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvCenario.Location = new System.Drawing.Point(3, 16);
             this.dgvCenario.Name = "dgvCenario";
-            this.dgvCenario.Size = new System.Drawing.Size(639, 158);
-            this.dgvCenario.TabIndex = 0;
             // 
             // btnExportar
             // 
-            this.btnExportar.Enabled = false;
+            resources.ApplyResources(this.btnExportar, "btnExportar");
             this.btnExportar.Image = global::TestManager.Properties.Resources._7182_32x32;
-            this.btnExportar.Location = new System.Drawing.Point(15, 425);
             this.btnExportar.Name = "btnExportar";
-            this.btnExportar.Size = new System.Drawing.Size(111, 44);
-            this.btnExportar.TabIndex = 4;
-            this.btnExportar.Text = "Exportar ";
-            this.btnExportar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnExportar.UseVisualStyleBackColor = true;
             this.btnExportar.Click += new System.EventHandler(this.btnExportar_ClickAsync);
             // 
             // btnSair
             // 
+            resources.ApplyResources(this.btnSair, "btnSair");
             this.btnSair.Image = global::TestManager.Properties.Resources._2934_32x32;
-            this.btnSair.Location = new System.Drawing.Point(546, 424);
             this.btnSair.Name = "btnSair";
-            this.btnSair.Size = new System.Drawing.Size(111, 44);
-            this.btnSair.TabIndex = 5;
-            this.btnSair.Text = "Sair";
-            this.btnSair.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnSair.UseVisualStyleBackColor = true;
             this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
             // 
             // frmFeature
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(669, 477);
             this.Controls.Add(this.btnSair);
             this.Controls.Add(this.btnExportar);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
             this.Name = "frmFeature";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Gerador de BDD";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmFeature_FormClosed);
             this.Load += new System.EventHandler(this.frmFeature_Load);
             this.groupBox1.ResumeLayout(false);
